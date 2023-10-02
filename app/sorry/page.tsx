36dcbfd10c59785266b68d8b162a4922718e93dd
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2023-10-02 08:54:29 +0700

    construct: under construction

---
+++ app/sorry/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SorryPage = () => {
+  return <div className="p-4">🙇‍♂️ Developer lagi benerin bug...</div>;
+};
+
+export default SorryPage;
--- middleware.ts
+++ middleware.ts
@@ -2,45 +2,47 @@ import { NextRequest, NextResponse } from "next/server";
 import { getAuthUser } from "./lib/helper/auth.helper";
 
 export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL("/forum?c=fyp", request.url));
-  }
+  return NextResponse.redirect(new URL("/sorry", request.url));
 
-  const isGuest =
-    request.nextUrl.pathname.startsWith("/login") ||
-    request.nextUrl.pathname.startsWith("/daftar");
-
-  const token = request.cookies.get("token");
-
-  if (!token?.value) {
-    if (isGuest) return NextResponse.next();
-
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  // When the Token exist
-  const payload = await getAuthUser(token.value);
-
-  if (!payload) {
-    if (isGuest) return NextResponse.next();
-
-    request.cookies.set("token", "");
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  // When the Payload is returning the auth user
-  if (isGuest || request.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL("/forum?c=fyp", request.url));
-  }
-
-  if (
-    request.nextUrl.pathname.startsWith("/reported-post") &&
-    payload.Role.name !== "developer"
-  ) {
-    return NextResponse.redirect(new URL("/forum?c=fyp", request.url));
-  }
-
-  return NextResponse.next();
+  // if (request.nextUrl.pathname === "/") {
+  //   return NextResponse.redirect(new URL("/forum?c=fyp", request.url));
+  // }
+  //
+  // const isGuest =
+  //   request.nextUrl.pathname.startsWith("/login") ||
+  //   request.nextUrl.pathname.startsWith("/daftar");
+  //
+  // const token = request.cookies.get("token");
+  //
+  // if (!token?.value) {
+  //   if (isGuest) return NextResponse.next();
+  //
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
+  //
+  // // When the Token exist
+  // const payload = await getAuthUser(token.value);
+  //
+  // if (!payload) {
+  //   if (isGuest) return NextResponse.next();
+  //
+  //   request.cookies.set("token", "");
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
+  //
+  // // When the Payload is returning the auth user
+  // if (isGuest || request.nextUrl.pathname === "/") {
+  //   return NextResponse.redirect(new URL("/forum?c=fyp", request.url));
+  // }
+  //
+  // if (
+  //   request.nextUrl.pathname.startsWith("/reported-post") &&
+  //   payload.Role.name !== "developer"
+  // ) {
+  //   return NextResponse.redirect(new URL("/forum?c=fyp", request.url));
+  // }
+  //
+  // return NextResponse.next();
 }
 
 export const config = {
